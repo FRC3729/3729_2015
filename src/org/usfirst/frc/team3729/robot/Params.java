447fe5b1b62ae71de871d2b4c82f6e437b54cbb1
first3729-dev <first3729@gmail.com>
2015-01-10 14:47:31 -0700

    Parameters file added

---
+++ src/org/usfirst/frc/team3729/robot/Params.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team3729.robot;
+
+public class Params {
+	//Port allocations
+    //!Talons
+    //!Relays
+	
+    //Joystick adjustments
+    //!Joy centers
+    //!Joy max/min
+    //!Exponential values
+    //!Max/Min robot speeds
+	
+    //Testing information
+    public static final boolean testing_drive = false;
+    public static final boolean testing_input = false;
+    
+}
